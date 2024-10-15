073cc8a05718ee56390505c28289ebf851fd6600
farhan2077 <farhanbinamin@gmail.com>
2024-10-16 00:10:12 +0600

    refactor: code structure

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+const baseUrl = "https://gutendex.com/books";
+
 const heartFilledSVG = `
 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#e11d48" stroke="#e11d48" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
   <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
@@ -18,7 +20,9 @@ function saveToWishlist(items) {
 
 function toggleFavorite(book) {
   let wishtlists = getWishlist();
-  const index = wishtlists.findIndex((favorite) => favorite.id === book.id);
+  const index = wishtlists.findIndex(
+    (wishlistedItem) => wishlistedItem.id === book.id
+  );
 
   if (index > -1) {
     wishtlists.splice(index, 1);
@@ -27,11 +31,74 @@ function toggleFavorite(book) {
   }
 
   saveToWishlist(wishtlists);
-  return wishtlists.some((favorite) => favorite.id === book.id);
+  return wishtlists.some((wishlistedItem) => wishlistedItem.id === book.id);
+}
+
+function createBookCard(bookList, book) {
+  const wishlists = getWishlist();
+
+  const card = document.createElement("div");
+  const cardBody = document.createElement("div");
+  card.className = "book-card";
+  cardBody.className = "book-card__body";
+
+  // wishlist
+  const wishlist = document.createElement("div");
+  wishlist.innerHTML = wishlists.some(
+    (wishlistedItem) => wishlistedItem.id === book.id
+  )
+    ? heartFilledSVG
+    : heartOutlineSVG;
+
+  wishlist.className = "book-card__wishlist-toggler";
+  wishlist.style.cursor = "pointer";
+
+  wishlist.onclick = function () {
+    const isFavorite = toggleFavorite(book);
+    this.innerHTML = isFavorite ? heartFilledSVG : heartOutlineSVG;
+  };
+
+  // title
+  const title = document.createElement("h2");
+  title.textContent = book.title;
+  title.className = "book-card__title";
+
+  // author
+  const authors = document.createElement("p");
+  authors.textContent = `by ${book.authors
+    .map((author) => author.name)
+    .join(", ")}`;
+  authors.className = "book-card__authors";
+
+  // cover image
+  const coverImg = document.createElement("img");
+  coverImg.src = book.formats["image/jpeg"];
+  coverImg.className = "book-card__img";
+
+  // genre
+  const genre = document.createElement("div");
+  genre.textContent = "classic fiction";
+  genre.className = "book-card__genre";
+
+  // id
+  const id = document.createElement("div");
+  id.textContent = `#${book.id}`;
+  id.className = "book-card__id";
+
+  cardBody.appendChild(title);
+  cardBody.appendChild(authors);
+  cardBody.appendChild(genre);
+  cardBody.appendChild(id);
+
+  card.appendChild(wishlist);
+  card.appendChild(coverImg);
+  card.appendChild(cardBody);
+
+  bookList.appendChild(card);
 }
 
 async function fetchData() {
-  const url = "https://gutendex.com/books";
+  let url = baseUrl;
 
   try {
     const response = await fetch(url);
@@ -40,72 +107,17 @@ async function fetchData() {
     }
 
     const data = await response.json();
+
     const loader = document.getElementById("loader-wrapper");
     loader.style.display = "none";
 
-    const favorites = getWishlist();
     const bookList = document.getElementById("book-cards__container");
+    bookList.innerHTML = "";
 
     data.results.forEach((book) => {
-      const card = document.createElement("div");
-      const cardBody = document.createElement("div");
-      card.className = "book-card";
-      cardBody.className = "book-card__body";
-
-      // wishlist
-      const wishlist = document.createElement("div");
-      wishlist.innerHTML = favorites.some((favorite) => favorite.id === book.id)
-        ? heartFilledSVG
-        : heartOutlineSVG;
-
-      wishlist.className = "book-card__wishlist-toggler";
-      wishlist.style.cursor = "pointer";
-
-      wishlist.onclick = function () {
-        const isFavorite = toggleFavorite(book);
-        this.innerHTML = isFavorite ? heartFilledSVG : heartOutlineSVG;
-      };
-
-      // title
-      const title = document.createElement("h2");
-      title.textContent = book.title;
-      title.className = "book-card__title";
-
-      // author
-      const authors = document.createElement("p");
-      authors.textContent = `by ${book.authors
-        .map((author) => author.name)
-        .join(", ")}`;
-      authors.className = "book-card__authors";
-
-      // cover image
-      const coverImg = document.createElement("img");
-      coverImg.src = book.formats["image/jpeg"];
-      coverImg.className = "book-card__img";
-
-      // genre
-      const genre = document.createElement("div");
-      genre.textContent = "classic fiction";
-      genre.className = "book-card__genre";
-
-      // id
-      const id = document.createElement("div");
-      id.textContent = `#${book.id}`;
-      id.className = "book-card__id";
-
-      cardBody.appendChild(title);
-      cardBody.appendChild(authors);
-      cardBody.appendChild(genre);
-      cardBody.appendChild(id);
-
-      card.appendChild(wishlist);
-      card.appendChild(coverImg);
-      card.appendChild(cardBody);
-
-      bookList.appendChild(card);
+      createBookCard(bookList, book);
     });
 
-    // Display the book list
     bookList.style.display = "block";
   } catch (error) {
     console.error("Error in fetchData:", error.message);
